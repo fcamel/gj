f350fd7bcef9a1cfb652fb9259126f88a251439a
fcamel <fcamel@gmail.com>
2014-03-27 15:01:00 +0800

    gj_util.py: fix bug that when the first argument is a number, there may be unmatched lines returned by gid.

--- bin/gj_util.py
+++ bin/gj_util.py
@@ -77,6 +77,9 @@ def find_matches(patterns=None, path_prefix=None):
     first_pattern = patterns[0]
 
     lines = _gid(first_pattern)
+    # gid may get unmatched pattern when the argument is a number.
+    # Don't know the reason. Manually filter unmatched lines.
+    lines = [line for line in lines if first_pattern in line]
     matches = [Match.create(line, first_pattern) for line in lines]
     matches = [m for m in matches if m]
 
